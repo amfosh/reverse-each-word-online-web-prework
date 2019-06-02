c07395f169571354aab2c503518b356d5c237a70
Alicia Fasciocco <afasciocco@gmail.com>
2019-06-02 18:44:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
-def reverse_each_word(string)
-  string.split(" ")
-  sentence.collect do |word| 
+def reverse_each_word(sentence)
+  sentence.split(" ")
+  array.collect do |word| 
   word.reverse
 end
   
