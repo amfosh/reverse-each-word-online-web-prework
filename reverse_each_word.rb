a7266f3a4d39a6d031fe2c2870bc855c2e0a3527
Alicia Fasciocco <afasciocco@gmail.com>
2019-06-02 18:54:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,2 @@
-def reverse_each_word(sentence)
-  sentence.each do |word|
-    word.split(" ")
-  end
-  words.collect do |word| 
-    word.reverse
-  end
-end
-    
+def reverse_each_word(array)
+  new_array = array.split(" ")
